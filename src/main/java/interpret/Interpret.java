e1ad3a90dd5586ccd104299dc3b96853c8777957
YohtaKimura <yohta.kimura@nts.ricoh.co.jp>
2020-01-20 18:24:01 +0900

    WIP

---
+++ src/main/java/interpret/Interpret.java
@@ -0,0 +1,10 @@
+package interpret;
+
+import javax.swing.*;
+
+public class Interpret {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("title");
+        frame.setVisible(true);
+    }
+}
