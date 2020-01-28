bea798babca27cdde412ef3283e3bbd512dae5ef
YohtaKimura <yohta.kimura@nts.ricoh.co.jp>
2020-01-28 20:40:23 +0900

    WIP

--- src/main/java/interpret/MethodsGetter.java
+++ src/main/java/interpret/MethodsGetter.java
@@ -7,8 +7,14 @@ import java.util.Optional;
 
 public class MethodsGetter {
     static Optional<Method[]> getMethods(final Object o) {
-        if(Objects.nonNull(o))
-        return Optional.of(o.getClass().getMethods());
+        if(Objects.nonNull(o)) {
+            return Optional.of(o.getClass().getMethods());
+        }
         return Optional.empty();
     }
+
+// see https://www.tutorialspoint.com/java/lang/class_getmethod.htm
+//    static Optional<Method> getMethodByName(final Object o, final String methodName) {
+//        getMethods(o);
+//    }
 }
